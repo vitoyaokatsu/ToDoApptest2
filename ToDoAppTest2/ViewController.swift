f6450cf947b8326ad8b4727e393647d83dac446f
vitoyaokatsu <vitoyaokatsu@gmail.com>
2020-09-22 21:08:33 +0900

    Datasource　関連をエクステンションでまとめた 今後テキストフィールドのデリゲートを整理

--- ToDoAppTest2/ViewController.swift
+++ ToDoAppTest2/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource{
+class ViewController: UIViewController, UITextFieldDelegate{
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -36,37 +36,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
         
     }
     
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        
-        true
-        
-    }
-    
-    
-    
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { // セルの数
-        return todos.count
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {// セクションの数
-        return 1
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {// セルの内容を決める。
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        let todo = todos[indexPath.row]
-        
-        cell.textLabel?.text = todo
-        
-        return cell
-    }
-    
-    
+
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {// returnキーを押した時の処理
         // 引数がテキストフィールドの値なのでシュッドリターンになるのか？？？
@@ -91,6 +61,51 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
         return true
     }
     
+
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print("you tapped me")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        true
+        
+    }
+    
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { // セルの数
+        return todos.count
+    }
+    
+    
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {// セクションの数
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {// セルの内容を決める。
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let todo = todos[indexPath.row]
+        
+        cell.textLabel?.text = todo
+        
+        return cell
+    }
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCell.EditingStyle.delete {
             todos.remove(at: indexPath.row)
@@ -106,11 +121,4 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
         self.tableView.reloadData() //データをリロードする
     }
     
-    
-}
-
-extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    print("you tapped me")
-    }
 }
